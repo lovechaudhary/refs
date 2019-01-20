80b194c4069c7e01c58216fede5563cadef4684f
DESKTOP-135PUJC\lovec <lovechaudhary17@gmail.com>
2019-01-20 18:40:49 -0500

    use by new technique

--- test-app/src/app/test/test.component.ts
+++ test-app/src/app/test/test.component.ts
@@ -1,19 +1,30 @@
 import { Component, OnInit } from "@angular/core";
 
+export interface Crew {
+  userId: number[];
+  crewId: number;
+  crewName: string;
+}
+
+export interface User {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: "app-test",
   templateUrl: "./test.component.html",
   styleUrls: ["./test.component.scss"]
 })
 export class TestComponent implements OnInit {
-  public crewData: object[] = [
+  public crewData: Crew[] = [
     { userId: [2], crewId: 1, crewName: "Apron" },
     { userId: [1], crewId: 2, crewName: "Runway" },
     { userId: [4], crewId: 3, crewName: "Taxiway" },
     { userId: [3], crewId: 4, crewName: "Hirzon" }
   ];
 
-  public userData: object[] = [
+  public userData: User[] = [
     { id: 1, name: "John" },
     { id: 2, name: "Mike" },
     { id: 3, name: "Bill" },
@@ -21,7 +32,10 @@ export class TestComponent implements OnInit {
   ];
 
   public crewClear: boolean;
-  constructor() {}
+
+  constructor() {
+    this.selectComparator = this.selectComparator.bind(this);
+  }
 
   ngOnInit() {
     this.crewClear = false;
@@ -38,5 +52,10 @@ export class TestComponent implements OnInit {
   onResetCrewClear() {
     this.crewClear = false;
     console.log(this.crewClear);
+  }
+
+  selectComparator(id) {
+    return +id === this.crewData.userId;
+    // instance from https://github.com/angular/material2/issues/2143
   }
 }
